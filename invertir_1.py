42d6f799bc3c12050315d877c456568a69fb3c86
Wilson Yaucán <75143213+wyaucan@users.noreply.github.com>
2021-12-29 15:30:43 -0500

    Add files via upload

---
+++ invertir_1.py
@@ -0,0 +1,6 @@
+
+#Invertir con método.
+
+cadena = "Hola Mundo"
+result_cad = " ".join(reversed(cadena))
+print(result_cad)
---
+++ invertir_2.py
@@ -0,0 +1,13 @@
+
+cadena = "Hola"
+#print(len(cadena))
+rever_cadena = ""
+i = len(cadena)
+
+while i > 0:
+    rever_cadena = cadena [i-1]
+    i = i-1
+    print(rever_cadena)
+
+
+    
